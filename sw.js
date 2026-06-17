1b9ce31b501d9adc504b71c74a814a944dc786cc
aleksey199010 <smileman19901@gmail.com>
2026-06-17 12:01:01 +0500

    add sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'govorilka-v1';
+const FILES = ['./speech-trainer.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES.filter(f => !f.endsWith('.png') || false)))
+      .catch(() => caches.open(CACHE).then(c => c.add('./speech-trainer.html')))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./speech-trainer.html')))
+  );
+});
